c97089907267b45988fb1fde8ffd06ce6d173a1a
Telly Teague <telly.teague@gmail.com>
2021-08-13 22:08:58 -0500

    complete exercise 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'My Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://image.shutterstock.com/image-illustration/side-view-studio-shot-white-260nw-191099207.jpg';
+  image3 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF_cuaEcdeg4lR0GRZ1CvvxA06A_J8LEwUog&usqp=CAU';
 
   constructor() { }
 

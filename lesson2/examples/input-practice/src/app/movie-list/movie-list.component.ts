f573362a70fe376524e090db160f0109186137ca
Telly Teague <telly.teague@gmail.com>
2021-08-20 20:38:59 -0500

    check in

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -5,11 +5,32 @@ import { Component, OnInit } from '@angular/core';
    templateUrl: './movie-list.component.html',
    styleUrls: ['./movie-list.component.css']
 })
-export class MovieListComponent implements OnInit {
+
+export class MovieListComponent implements OnInit 
+{
    movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
 
    constructor() { }
 
    ngOnInit() {
+   }
+
+   addMovie( newTitle: string )
+   {
+     let msg: string = "";
+     if ( newTitle.trim().length === 0 )
+     {
+       msg = "Invalid value.";
+     }
+     else if ( this.movies.includes(newTitle) )
+     {
+       msg = "We love that movie too, but no repeats!";
+     }
+
+     if ( msg.trim().length === 0 )
+     {
+       this.movies.push( newTitle );
+     }
+     return msg;
    }
 }
